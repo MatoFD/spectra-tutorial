5956c11e391ad6be490956d438a198fd5f45cf7f
mato <matiasfeduran@gmail.com>
2021-08-17 12:50:24 -0300

    remove old cluter

--- D1_parsing/src/parsing/SpectraToMTSATtranslator.java
+++ D1_parsing/src/parsing/SpectraToMTSATtranslator.java
@@ -66,30 +66,9 @@ public class SpectraToMTSATtranslator {
 			// parse (via Xtext) and translate to abstract syntax (Xtext independent)
 			GameInput gi = sip.getGameInput(specPath);
 	
-	//		System.out.println("\nPrinting vars and constraints of system player:");
-	//		Player sys = gi.getSys();
-	//		printVars(sys);
-	//		printConstraints(sys);
-	//		
-	//		System.out.println("Printing vars and constraints of environment player:");
-	//		Player env = gi.getEnv();
-	//		printVars(env);
-	//		printConstraints(env);
-	
 			System.out.println("\nTranslating " +name+ " to Spectra Kernel.");
 			// important step to reduce language features to the Spectra Kernel
 			TranslationProvider.translate(gi);
-	//		Player aux = gi.getAux();
-	
-	//		System.out.println("\nPrinting vars and constraints of system and aux players:");
-	//		printVars(sys);
-	//		printVars(aux);
-	//		printConstraints(sys);
-	//		printConstraints(aux);
-			
-	//		System.out.println("\nPrinting vars and constraints of environment player:");
-	//		printVars(env);
-	//		printConstraints(env);
 			
 			translateToFSP(name, gi);
 		}
@@ -360,36 +339,4 @@ public class SpectraToMTSATtranslator {
 		String ans = actions.stream().collect(Collectors.joining(", "));
 		out.println("set "+name+" = {"+ans+"}");
 	}
-
-	
-	private static void printVars(Player p) {
-		for (Variable v : p.getVars()) {
-			System.out.println(v.toString());
-		}
-	}
-
-	private static void printConstraints(Player p) {
-		for (Constraint c : p.getConstraints()) {
-			switch (c.getKind()) {
-			case INI:
-				System.out.println("Initial constraint " + c.getSpec());
-				break;
-			case SAFETY:
-				System.out.println("Safety constraint " + c.getSpec());
-				break;
-			case STATE_INV:
-				System.out.println("State invariant " + c.getSpec());
-				break;
-			case JUSTICE:
-				System.out.println("Justice constraint " + c.getSpec());
-				break;
-			case PATTERN:
-				System.out.println("Pattern constraint " + c.getSpec());
-				break;
-			default:
-				break;
-			}
-		}
-	}
-
 }
